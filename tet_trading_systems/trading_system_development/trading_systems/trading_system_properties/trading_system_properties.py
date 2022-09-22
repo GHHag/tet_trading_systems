10f78ddffd12c72966307c85c3d467a068a439b2
GHHag <gustav.hagenblad@gmail.com>
2022-09-22 16:07:26 +0200

    .

--- tet_trading_systems/trading_system_development/trading_systems/trading_system_properties/trading_system_properties.py
+++ tet_trading_systems/trading_system_development/trading_systems/trading_system_properties/trading_system_properties.py
@@ -1,12 +1,22 @@
+from dataclasses import dataclass
 from typing import Tuple, Callable
 
 from tet_trading_systems.trading_system_state_handler.trad_trading_system_state_handler import TradingSystemStateHandler
 from tet_trading_systems.trading_system_state_handler.portfolio.portfolio import Portfolio
 
 
+@dataclass(frozen=True)
 class TradingSystemProperties:
+    preprocess_data_function: Callable
+    preprocess_data_args: Tuple
+    system_state_handler: TradingSystemStateHandler
+    system_state_handler_args: Tuple
+    system_state_handler_call_args: Tuple
+    portfolio: Portfolio
+    portfolio_args: Tuple
+    portfolio_call_args: Tuple
 
-    def __init__(
+    """ def __init__(
         self,
         preprocess_data_function: Callable, preprocess_data_args: Tuple, 
         system_state_handler: TradingSystemStateHandler, system_state_handler_args: Tuple, 
@@ -52,4 +62,4 @@ class TradingSystemProperties:
 
     @property
     def portfolio_call_args(self):
-        return self.__portfolio_call_args
+        return self.__portfolio_call_args """
